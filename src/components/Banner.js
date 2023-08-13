9775a58a79407e0ab0a6be71ae9170a18dd2c507
Tyler Weifenbach <rweifenbach24@gmail.com>
2023-08-13 18:21:25 -0500

    updated Banner and apps with banner

---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+export const Banner = () => {
+    return (
+        <div>
+            <header>Orbit Report</header>
+            <p>Click on the buttons to see satellites in that orbit type.</p>
+        </div>       
+    )
+}
